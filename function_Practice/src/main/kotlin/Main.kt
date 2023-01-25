491599513be7178a8a9bfd0adce90c30c0ae1d0d
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-01-25 11:51:38 +0530

    Prac 2: Movie-ticket price

--- function_Practice/src/main/kotlin/Main.kt
+++ function_Practice/src/main/kotlin/Main.kt
@@ -1,21 +1,37 @@
 fun main() {
 
-    val morningNotification = 51
-    val eveningNotification = 135
+    val child = 5
+    val adult = 28
+    val senior = 87
 
-    printNumberOfSummaryNotification(morningNotification)
-    printNumberOfSummaryNotification(eveningNotification)
+
+    val isMonday = true
+
+
+    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+
+
 }
 
-fun printNumberOfSummaryNotification(numberOfMessages : Int){
+fun ticketPrice(age: Int, isMonday: Boolean): Any {
 
-    if(numberOfMessages >= 100){
-        println("Your phone is blowing up! You have 99+ notifications.")
-    }else {
-        println("You have $numberOfMessages notifications.")
+    var price: Int = 0
+
+    price = when {
+        age in 1..12 -> 15
+        isMonday && age in 13..60 -> 25
+        age in 13..60 -> 30
+        age in 61..100 -> 20
+        else -> -1
     }
+    return price
+
 }
+
 /*
-You have 51 notifications.
-Your phone is blowing up! You have 99+ notifications.
+The movie ticket price for a person aged 5 is  $15.
+The movie ticket price for a person aged 28 is $25.
+The movie ticket price for a person aged 87 is $20.
  */
