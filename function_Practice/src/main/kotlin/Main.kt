090c6e34b2bffe8b090e99a74dbbfaa8a41eb914
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-01-25 12:44:40 +0530

    Prac 3.1: Updated code P3

--- function_Practice/src/main/kotlin/Main.kt
+++ function_Practice/src/main/kotlin/Main.kt
@@ -1,20 +1,16 @@
 fun main() {
-    val child = 5
-    val adult = 28
-    val senior = 87
-
-    val isMonday = true
-
-    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.")
-    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
-    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+    printFinalTemperature(27.0, "Celsius", "Fahrenheit") { 9.0 / 5.0 * it + 32 }
+    printFinalTemperature(350.0, "Kelvin", "Celsius") { it - 273.15 }
+    printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { 5.0 / 9.0 * (it - 32) + 273.15 }
 }
 
-fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    return when(age) {
-        in 0..12 -> 15
-        in 13..60 -> if (isMonday) 25 else 30
-        in 61..100 -> 20
-        else -> -1
-    }
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
