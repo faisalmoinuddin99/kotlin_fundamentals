e94465a7f6e3d05e80551965aa773227656e422e
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-01-25 12:43:03 +0530

    Prac 2.1 : Updated code for P2

--- function_Practice/src/main/kotlin/Main.kt
+++ function_Practice/src/main/kotlin/Main.kt
@@ -1,26 +1,20 @@
 fun main() {
-    // Fill in the code.
-    // available temp : Celsius, Fahrenheit, Kelvin
-    val temp : Double = 350.0
-    val from : String = "Kelvin"
-    val to : String = "Celsius"
-    printFinalTemperature(temp,from,to){
-        when {
-            from == "Celsius" && to == "Fahrenheit" -> (temp * 9/5) + 32
-            from == "Fahrenheit" && to == "Kelvin" -> (temp - 32) * 5/9 + 273.15
-            from == "Kelvin" && to == "Celsius" -> temp - 273.15
-            else -> 0.0
-        }
-    }
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
 }
 
-
-fun printFinalTemperature(
-    initialMeasurement: Double,
-    initialUnit: String,
-    finalUnit: String,
-    conversionFormula: (Double) -> Double
-) {
-    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
-    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
+    }
 }
