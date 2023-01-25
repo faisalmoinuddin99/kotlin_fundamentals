2a0cffbb7dedd6defc25ee95fca1276454d86d21
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-01-25 12:39:07 +0530

    Prac 3: Temperature converter

--- function_Practice/src/main/kotlin/Main.kt
+++ function_Practice/src/main/kotlin/Main.kt
@@ -1,37 +1,26 @@
 fun main() {
-
-    val child = 5
-    val adult = 28
-    val senior = 87
-
-
-    val isMonday = true
-
-
-    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.")
-    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
-    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
-
-
-}
-
-fun ticketPrice(age: Int, isMonday: Boolean): Any {
-
-    var price: Int = 0
-
-    price = when {
-        age in 1..12 -> 15
-        isMonday && age in 13..60 -> 25
-        age in 13..60 -> 30
-        age in 61..100 -> 20
-        else -> -1
+    // Fill in the code.
+    // available temp : Celsius, Fahrenheit, Kelvin
+    val temp : Double = 350.0
+    val from : String = "Kelvin"
+    val to : String = "Celsius"
+    printFinalTemperature(temp,from,to){
+        when {
+            from == "Celsius" && to == "Fahrenheit" -> (temp * 9/5) + 32
+            from == "Fahrenheit" && to == "Kelvin" -> (temp - 32) * 5/9 + 273.15
+            from == "Kelvin" && to == "Celsius" -> temp - 273.15
+            else -> 0.0
+        }
     }
-    return price
-
 }
 
-/*
-The movie ticket price for a person aged 5 is  $15.
-The movie ticket price for a person aged 28 is $25.
-The movie ticket price for a person aged 87 is $20.
- */
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
