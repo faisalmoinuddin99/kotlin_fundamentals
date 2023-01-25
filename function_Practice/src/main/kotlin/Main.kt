7d0cfa9e3d9fa52f90505c1884c7069f677af16d
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-01-25 11:18:01 +0530

    Prac 1: Mobile Notification

--- function_Practice/src/main/kotlin/Main.kt
+++ function_Practice/src/main/kotlin/Main.kt
@@ -1,46 +1,21 @@
 fun main() {
 
-//    val coins: (Int) -> String = {
-//        "$it quarter"
-//    }
+    val morningNotification = 51
+    val eveningNotification = 135
 
-    val cupCake: (Int) -> String = {
-        "Have a cupcake !"
-    }
-
-    // trailing lambda syntax, we don't need to pass coin as reference variable
-    val human1 = testingFunctionality(true) {
-        "$it quarter"
-    }
-
-    /* repeat is a high-order function, which uses the for loop behind the hood
-    also it uses trailing lambda syntax
-     */
-    repeat(5){
-        human1()
-    }
-
-
-
+    printNumberOfSummaryNotification(morningNotification)
+    printNumberOfSummaryNotification(eveningNotification)
 }
 
-fun testingFunctionality(isVerified: Boolean, extraFeature: ((Int) -> String)?): () -> Unit {
-    return if (isVerified) {
-        // execute extraFeature
-        if (extraFeature != null) {
-            println(extraFeature(5))
-        }
-        welcomeMessage
+fun printNumberOfSummaryNotification(numberOfMessages : Int){
 
-    } else {
-        message
+    if(numberOfMessages >= 100){
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }else {
+        println("You have $numberOfMessages notifications.")
     }
 }
-
-val welcomeMessage: () -> Unit = {
-    println("Welcome Sir, please sit here...")
-}
-
-val message: () -> Unit = {
-    println("Thanking you, will see you again.... 🙂")
-}
+/*
+You have 51 notifications.
+Your phone is blowing up! You have 99+ notifications.
+ */
